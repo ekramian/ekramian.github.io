50f1310093b82617614105ceee5a5ed86327001f
Ali Ekramian <ali.ekramiyan@gmail.com>
2014-11-07 15:03:26 +0330

    Internet

---
+++ js/authors.js
@@ -0,0 +1,77 @@
+var authors={tanzad:{html:'<div class="author-bio">'+'<p>وبلاگ نویس آی‌تی، علاقه‌مند به مباحث فناوری، تا اندازه‌ای طراح وب و یک سری موارد دیگر!</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/arashzd"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/arash.zaad"><img src="/static/img/social/facebook.png"></a> \
+                <a target="_blank" href="http://ir.linkedin.com/in/arashzaad"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/arash.zad.png'},arman:{html:'<div class="author-bio">'+'<p>وبلاگ‌نویس آی‌تی</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/armanhoseini"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/armanhoseini"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/arman.hosseini.png'},alireza:{html:'<div class="author-bio">'+'<p>طراح وب و گوینده پادکست</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/alihaa"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/ali.jamalzade"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/ali.jamalzadeh.png'},arad:{html:'<div class="author-bio">'+'<p>متخصص شبکه و برنامه‌نویس دات نت</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/AradHaghi"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/aradhaghi"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/arad.haghi.png'},farhad:{html:'<div class="author-bio">'+'<p>وبلاگ‌نویس آی‌تی</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/farhadeidi"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/farhadeidi"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="http://ir.linkedin.com/pub/farhad-eidi/43/a93/a71"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/farhad.eidi.png'},pouya:{html:'<div class="author-bio">'+'<p>پشتیبان فنی و برنامه‌نویس</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/pouya"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/Pouya.Salimi"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/pouya.salimi.png'},soodabeh:{html:'<div class="author-bio">'+'<p>گرافیست و طراح وب</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/soodabe1"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/soodabe"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/soodabe.amirakbari.png'},jimmyheller:{html:'<div class="author-bio">'+'<p>برنامه‌نویس، وبلاگ‌نویس و عاشق تکنولوژی‌های جدید</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/jimmyheller"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/JimmyHeller"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="http://ir.linkedin.com/pub/dir/Jimmy/Heller"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/ali-azimipour.jpg'},mesut:{html:'<div class="author-bio">'+'<p>طراح وب و برنامه‌نویس دات نت</p>'+'<span> \
+                <a target="_blank" href="https://twitter.com/#!/mesutdarvishian"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/darvishian"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="http://www.linkedin.com/in/mesutdarvishian"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/mesut.jpg'},malek:{html:'<div class="author-bio">'+'<p>عضوی از وبلاگستان فارسی، دانشجوی مکانیک، مجسمه ساز و کمی هم گیمر!</p>'+'<span> \
+                <a target="_blank" href="#"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="#"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/malek.jpg'},maboodi:{html:'<div class="author-bio">'+'<p>وبلاگ‌نویس فناوری</p>'+'<span> \
+                <a target="_blank" href="#"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="#"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/images/default.jpg'},bidari:{html:'<div class="author-bio">'+'<p>مترجم و نویسنده آی‌تی و علاقه‌مند به فناوری</p>'+'<span> \
+                <a target="_blank" href="#"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="#"><img src="/static/img/social/facebook.png"></a> \
+                <a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/forough.bidari.jpg'},marzieh:{html:'<div class="author-bio">'+'<p>فعالیت در زمینه هدایت پروژه، پژوهش در محیط وب و مدیریت ارتباط با مشتریان</p>'+'<span> \
+                <a target="_blank" href="#"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="#"><img src="/static/img/social/facebook.png"></a> \
+                <a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/marzieh.jpg'},parham:{html:'<div class="author-bio">'+'<p>وبلاگ نویس</p>'+'<span> \
+                <a target="_blank" href="https://twitter.com/parhamb"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="#"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="http://ir.linkedin.com/in/parhamb"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/parham.jpg'},reza:{html:'<div class="author-bio">'+'<p>دانشجوی مهندسی نرم افزار، علاقمند به فناوری و تا حدودی برنامه‌نویس</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/satrap92"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/reza.hamouleh"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/reza.jpg'},maryam:{html:'<div class="author-bio">'+'<p>ایده‌پرداز، مدیر پروژه کفشدوزک</p>'+'<span> \
+                <a target="_blank" href="http://twitter.com/maryamansari_"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/maryamansari.90"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="http://www.linkedin.com/in/maryamansari"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/maryam.jpg'},kamelan:{html:'<div class="author-bio">'+'<p>مترجم زبان انگلیسی و فعال در حوزه فن آوری اطلاعات</p>'+'<span> \
+                <a target="_blank" href="#"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="#"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/mt/mt-static/support/assets_c/userpics/userpic-24-100x100.png'},mani:{html:'<div class="author-bio">'+'<p>وبلاگ نویس حوزه فناوری و موبایل</p>'+'<span> \
+                <a target="_blank" href="https://twitter.com/manigh"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="#"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="#"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/mani2.jpg'},saeed:{html:'<div class="author-bio">'+'<p>وبلاگ نویس حوزه وب و فناوری ایران</p>'+'<span> \
+                <a target="_blank" href="https://twitter.com/saeed_choobani"><img src="/static/img/social/twitter.png"></a> \
+                <a target="_blank" href="https://www.facebook.com/saeed.choobani"><img src="/static/img/social/facebook.png"></a> \
+				<a target="_blank" href="http://ir.linkedin.com/in/saeedchoobani‎"><img src="/static/img/social/linkedin.png"></a> \
+            </span>'+'</div>',img:'http://weblogina.com/about/images/saeed.jpg'},};
